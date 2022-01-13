0bdbe08b617679eaf85aa05f2463e4e7542d3290
k123s456h <k123s456h@gmail.com>
2022-01-13 22:19:07 +0900

    *3*

---
+++ main.example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+
+using namespace std;
+
+class Solution
+{
+
+};
+
+int main(int argc, char* argv[])
+{
+  setbuf(stdout, NULL);
+  freopen("input.txt", "r", stdin);
+  
+  int TC;
+  Solution solution;
+  scanf("%d", &TC);
+  while(TC--)
+  {
+    
+  }
+
+  return 0;
+}
